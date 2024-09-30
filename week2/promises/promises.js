70a93563de677d0b635cb6acf17f76ce8b930b82
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-09-30 20:57:29 +0530

    promises More

--- week2/promises/promises.js
+++ week2/promises/promises.js
@@ -1,9 +1,39 @@
-function setTimeoutPromisified(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+// function setTimeoutPromisified(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// function callback() {
+//   console.log("3 Seconds Done !");
+// }
+
+// setTimeoutPromisified(3000).then(callback);
+
+
+// function promiseCallback(resolve){
+//     setTimeout(resolve,3000)
+
+// }
+
+// promiseCallback(function(){
+//     console.log("hi");
+// })
+
+
+
+
+// setTimeout(()=>{
+//     console.log("Hi Nextb time");
+// },3000)
+
+
+
+
+function setTimefor3s(resolve){
+    setTimeout(resolve,3000)
 }
 
-function callback() {
-  console.log("3 Seconds Done !");
+function main(){
+    console.log("Main Is Called");
 }
 
-setTimeoutPromisified(3000).then(callback);
+setTimefor3s(main)
