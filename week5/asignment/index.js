08d96d365919a1af0a8faf7648ceb47b91855521
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-10-10 18:22:23 +0530

    Proceeded

--- week5/asignment/index.js
+++ week5/asignment/index.js
@@ -1,12 +1,68 @@
 const express = require("express");
 const app = express();
-const port =  4000
+const port =  3000
 
 app.get("/",(req,res)=>{
     res.json({
-        "message":"Hi"
+        "Message":"Welcome"
+    })
+})
+
+app.get("/sum",(req,res)=>{
+    const a  = parseInt(req.query.a);
+    const b =  Number(req.query.b); // Number() -> converts string to integer
+    res.json({
+        "sum Is :":a+b
+    })
+})
+app.get("/dif/:a/:b",(req,res)=>{
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+    res.json({
+        "Diffrence Is :":a-b
+    })
+})
+
+app.get("/multyply",(req,res)=>{
+    const a  = req.query.a;
+    const b =  req.query.b;
+    res.json({
+        "multyply Is :":a*b
     })
 });
+
+app.get("/multyply",(req,res)=>{
+    const a  = req.query.a;
+    const b =  req.query.b;
+    res.json({
+        "multyply Is :":a*b
+    })
+})
+app.get("/multyply",(req,res)=>{
+    const a  = req.query.a;
+    const b =  req.query.b;
+    res.json({
+        "multyply Is :":a*b
+    })
+})
+
+app.get("/multyply",(req,res)=>{
+    const a  = req.query.a;
+    const b =  req.query.b;
+    res.json({
+        "multyply Is :":a*b
+    })
+})
+
+app.get("/div",(req,res)=>{
+    const a  = req.query.a;
+    const b =  req.query.b;
+    res.json({
+        "Division Is :":a/b
+    })
+})
+
+
 
 
 app.listen(port,()=>{
