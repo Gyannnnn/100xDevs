009a871625cdeaddc3b9237a29d111414579b480
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-10-12 10:21:34 +0530

    Authentication

--- week6/authentication/index.js
+++ week6/authentication/index.js
@@ -1,11 +1,63 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
+const users = [];
+
+function generateRandomString(){
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    
+    for (let i = 0; i < charactersLength; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  
+    return result;
+}
 
 app.get("/",(req,res)=>{
     res.json({
         "Message":"Home Page"
     })
+})
+app.post("/signup",(req,res)=>{
+    const userName = req.body.userName;
+    const userPassword = req.body.userPassword;
+
+    users.push({
+        userName:userName,
+        userPassword:userPassword
+    });
+    res.json({
+        message:"SignedUp Successfully"
+    });
+
+
+    
+})
+
+app.post("/signin",(req,res)=>{
+  const userName = req.body.userName;
+  const userPassword = req.body.userPassword;
+
+  const foundUser = users.find((u) => {
+    return userName == u.userName && userPassword == u.userPassword;
+  });
+
+  if (foundUser) {
+    const token = generateRandomString();  // Generate a random token of 16 characters
+    foundUser.token = token;
+    res.json({
+      token: token
+    });
+  } else {
+    return res.json({
+      message: "Invalid User Credential"
+    });
+  }
+
 })
 
 app.listen(3000,()=>{
