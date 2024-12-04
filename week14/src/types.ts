9ce518e10b2c72d6a09b81fb0a4403cc368e869b
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-04 22:15:13 +0530

    Types Continued

--- week14/src/types.ts
+++ week14/src/types.ts
@@ -1,13 +1,51 @@
 // types let us to aggregate the data together
 
 
-interface User{
+// interface User{
+//     name: string;
+//     age: number;
+// }
+
+
+// type User1 = {
+//     name: string;
+//     age: number ;
+// }
+
+
+
+// function isLegal(user: User1): boolean{
+//     return user.age>18
+
+// }
+
+
+
+type employee = {
     name: string;
-    age: number;
+    startDate: string;   
+};
+
+type manager = {
+    name: string;
+    department: string
+};
+
+let e: employee = {
+    name: "Harkrat",
+    startDate: "12-23-12222"
 }
 
+let ma: manager = {
+    name: "harkirat",
+    department: "Electricity",
+}
 
-type User1 = {
-    name: string;
-    age: number 
+type TeamLead = employee & manager ;
+
+let tld: TeamLead = {
+    name: "Gyanaranjan Patra",
+    startDate: "90-12-22222",
+    department:"Chor",
+    
 }
