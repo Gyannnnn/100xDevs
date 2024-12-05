52c894f9d1d113312ad9ecf9cad84b0cf1450b10
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-05 14:48:41 +0530

    Arrays In TS

---
+++ week14/src/ArrayAsingment.ts
@@ -0,0 +1,133 @@
+interface User12 {
+  FirstName: string;
+  LastName: string;
+  age: number;
+}
+
+let users: User12[] = [
+    { FirstName: "Gyanranjan", LastName: "Patra", age: 21 },
+    { FirstName: "Another", LastName: "User", age: 17 },
+    { FirstName: "Harkirat", LastName: "Singh", age: 23 },
+    { FirstName: "User4", LastName: "LastName4", age: 25 },
+    { FirstName: "User5", LastName: "LastName5", age: 29 },
+    { FirstName: "User6", LastName: "LastName6", age: 34 },
+    { FirstName: "User7", LastName: "LastName7", age: 22 },
+    { FirstName: "User8", LastName: "LastName8", age: 30 },
+    { FirstName: "User9", LastName: "LastName9", age: 27 },
+    { FirstName: "User10", LastName: "LastName10", age: 19 },
+    { FirstName: "User11", LastName: "LastName11", age: 40 },
+    { FirstName: "User12", LastName: "LastName12", age: 31 },
+    { FirstName: "User13", LastName: "LastName13", age: 36 },
+    { FirstName: "User14", LastName: "LastName14", age: 24 },
+    { FirstName: "User15", LastName: "LastName15", age: 21 },
+    { FirstName: "User16", LastName: "LastName16", age: 23 },
+    { FirstName: "User17", LastName: "LastName17", age: 38 },
+    { FirstName: "User18", LastName: "LastName18", age: 5 },
+    { FirstName: "User19", LastName: "LastName19", age: 25 },
+    { FirstName: "User20", LastName: "LastName20", age: 35 },
+    { FirstName: "User21", LastName: "LastName21", age: 29 },
+    { FirstName: "User22", LastName: "LastName22", age: 26 },
+    { FirstName: "User23", LastName: "LastName23", age: 33 },
+    { FirstName: "User24", LastName: "LastName24", age: 28 },
+    { FirstName: "User25", LastName: "LastName25", age: 4 },
+    { FirstName: "User26", LastName: "LastName26", age: 16 },
+    { FirstName: "User27", LastName: "LastName27", age: 20 },
+    { FirstName: "User28", LastName: "LastName28", age: 32 },
+    { FirstName: "User29", LastName: "LastName29", age: 23 },
+    { FirstName: "User30", LastName: "LastName30", age: 34 },
+    { FirstName: "User31", LastName: "LastName31", age: 24 },
+    { FirstName: "User32", LastName: "LastName32", age: 36 },
+    { FirstName: "User33", LastName: "LastName33", age: 28 },
+    { FirstName: "User34", LastName: "LastName34", age: 27 },
+    { FirstName: "User35", LastName: "LastName35", age: 25 },
+    { FirstName: "User36", LastName: "LastName36", age: 31 },
+    { FirstName: "User37", LastName: "LastName37", age: 22 },
+    { FirstName: "User38", LastName: "LastName38", age: 21 },
+    { FirstName: "User39", LastName: "LastName39", age: 29 },
+    { FirstName: "User40", LastName: "LastName40", age: 40 },
+    { FirstName: "User41", LastName: "LastName41", age: 20 },
+    { FirstName: "User42", LastName: "LastName42", age: 35 },
+    { FirstName: "User43", LastName: "LastName43", age: 24 },
+    { FirstName: "User44", LastName: "LastName44", age: 23 },
+    { FirstName: "User45", LastName: "LastName45", age: 38 },
+    { FirstName: "User46", LastName: "LastName46", age: 25 },
+    { FirstName: "User47", LastName: "LastName47", age: 3 },
+    { FirstName: "User48", LastName: "LastName48", age: 32 },
+    { FirstName: "User49", LastName: "LastName49", age: 30 },
+    { FirstName: "User50", LastName: "LastName50", age: 33 },
+    { FirstName: "User51", LastName: "LastName51", age: 27 },
+    { FirstName: "User52", LastName: "LastName52", age: 26 },
+    { FirstName: "User53", LastName: "LastName53", age: 34 },
+    { FirstName: "User54", LastName: "LastName54", age: 22 },
+    { FirstName: "User55", LastName: "LastName55", age: 29 },
+    { FirstName: "User56", LastName: "LastName56", age: 19 },
+    { FirstName: "User57", LastName: "LastName57", age: 31 },
+    { FirstName: "User58", LastName: "LastName58", age: 36 },
+    { FirstName: "User59", LastName: "LastName59", age: 25 },
+    { FirstName: "User60", LastName: "LastName60", age: 24 },
+    { FirstName: "User61", LastName: "LastName61", age: 28 },
+    { FirstName: "User62", LastName: "LastName62", age: 21 },
+    { FirstName: "User63", LastName: "LastName63", age: 20 },
+    { FirstName: "User64", LastName: "LastName64", age: 7 },
+    { FirstName: "User65", LastName: "LastName65", age: 23 },
+    { FirstName: "User66", LastName: "LastName66", age: 22 },
+    { FirstName: "User67", LastName: "LastName67", age: 19 },
+    { FirstName: "User68", LastName: "LastName68", age: 35 },
+    { FirstName: "User69", LastName: "LastName69", age: 33 },
+    { FirstName: "User70", LastName: "LastName70", age: 27 },
+    { FirstName: "User71", LastName: "LastName71", age: 36 },
+    { FirstName: "User72", LastName: "LastName72", age: 25 },
+    { FirstName: "User73", LastName: "LastName73", age: 38 },
+    { FirstName: "User74", LastName: "LastName74", age: 30 },
+    { FirstName: "User75", LastName: "LastName75", age: 26 },
+    { FirstName: "User76", LastName: "LastName76", age: 37 },
+    { FirstName: "User77", LastName: "LastName77", age: 34 },
+    { FirstName: "User78", LastName: "LastName78", age: 21 },
+    { FirstName: "User79", LastName: "LastName79", age: 23 },
+    { FirstName: "User80", LastName: "LastName80", age: 5 },
+    { FirstName: "User81", LastName: "LastName81", age: 20 },
+    { FirstName: "User82", LastName: "LastName82", age: 32 },
+    { FirstName: "User83", LastName: "LastName83", age: 29 },
+    { FirstName: "User84", LastName: "LastName84", age: 24 },
+    { FirstName: "User85", LastName: "LastName85", age: 18 },
+    { FirstName: "User86", LastName: "LastName86", age: 33 },
+    { FirstName: "User87", LastName: "LastName87", age: 36 },
+    { FirstName: "User88", LastName: "LastName88", age: 38 },
+    { FirstName: "User89", LastName: "LastName89", age: 40 },
+    { FirstName: "User90", LastName: "LastName90", age: 31 },
+    { FirstName: "User91", LastName: "LastName91", age: 26 },
+    { FirstName: "User92", LastName: "LastName92", age: 35 },
+    { FirstName: "User93", LastName: "LastName93", age: 30 },
+    { FirstName: "User94", LastName: "LastName94", age: 25 },
+    { FirstName: "User95", LastName: "LastName95", age: 17 },
+    { FirstName: "User96", LastName: "LastName96", age: 20 },
+    { FirstName: "User97", LastName: "LastName97", age: 28 },
+    { FirstName: "User98", LastName: "LastName98", age: 22 },
+    { FirstName: "User99", LastName: "LastName99", age: 29 },
+    { FirstName: "User100", LastName: "LastName100", age: 37 },
+  ];
+  
+
+function isLegal(users: User12[]) {
+    let adult = []
+    let kids = []
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age > 18) {
+      adult.push(users[i])
+    }else{
+        kids.push(users[i]);
+    }
+  }
+  console.log("Users Which can vote are");
+  for(let i=0;i< adult.length;i++){
+    console.log(adult[i].FirstName+ " Age : "+ adult[i].age  );
+  }
+  console.log("Users Which can not vote are");
+  for(let i=0;i< kids.length;i++){
+    console.log(kids[i].FirstName+ " Age :"+kids[i].age);
+  }
+}
+
+
+
+isLegal(users);
--- week14/src/Arrays.ts
+++ week14/src/Arrays.ts
@@ -1,1 +1,32 @@
-console.log("Arrays ............................................");
+function maxValue(nums: number[]): number {
+  let maxValue = -1234445;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > maxValue) {
+      maxValue = nums[i];
+    }
+  }
+  return maxValue;
+}
+
+let nm = maxValue([1, 2, 3, 4, 5, 6, 67, 7, 123]);
+
+console.log(nm);
+
+interface Address {
+  city: string;
+  pincode: string;
+}
+
+interface User00 {
+  name: string;
+  age: number;
+  addresses: Address[];
+}
+
+let user000: User00 = {
+  name: "Gyanaranjan Patra",
+  age: 21,
+  addresses: [],
+};
+ 
