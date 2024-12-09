d32dafd2279f06fef2f4a5730ceb55a7f06ab79e
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-09 14:34:34 +0530

    final commit of part1

--- week17_postgress/part1/src/index.ts
+++ week17_postgress/part1/src/index.ts
@@ -12,7 +12,6 @@ const pgClient = new Client({
   database: "neondb",
   ssl: true
 });
-// postgresql://neondb_owner:vE3NX8nscdFS@ep-ancient-butterfly-a518kar0.us-east-2.aws.neon.tech/neondb?sslmode=require
 
 pgClient.connect();
 
