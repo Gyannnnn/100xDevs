a2cfae0768a655e8dc3bb0e5e12b5b8ccd953299
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-17 15:09:00 +0530

    Server Action

--- nextjs/fsapp/app/api/v1/signup/route.ts
+++ nextjs/fsapp/app/api/v1/signup/route.ts
@@ -1,8 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient()
 
 export async function POST(req:NextRequest){
     const data = await req.json()
     console.log(data)
+
+   await client.user.create({
+        data:{
+            username: data.username,
+            password: data.password
+        }
+    })
+
+
+
     return NextResponse.json({
         message:"Signed Up Successfull"
     })
