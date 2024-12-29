e0f9b0cc5fac4251e6bcb772f749f2d6ae3e958c
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-29 20:59:04 +0530

    initial setup

---
+++ nextjs/nextauth/app/page.tsx
@@ -0,0 +1,13 @@
+import { getServerSession } from "next-auth"
+
+export default async function page() {
+  const session = await getServerSession()
+
+  return (
+    <div>
+      {
+        JSON.stringify(session)
+      }
+    </div>
+  )
+}
--- nextjs/part4/app/page.tsx
+++ nextjs/part4/app/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 export default function page() {
+
+const [cout,Setcount] = useState(0)
+const time = Date.now()
+
+
   return (
-    <div>Hi There</div>
+    <div>
+      {time}
+      <button onClick={()=>Setcount(c => c+1)}>Click Me{cout}</button>
+    </div>
   )
 }
